04fcad67d5486efffdde6c454aa4e18fa40cfa10
shaneruddle <shaneruddle@gmail.com>
2026-05-22 12:55:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,31 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.NODE_ENV': JSON.stringify(mode),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true
-    },
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+    return {
+          plugins: [react(), tailwindcss()],
+          define: {
+                  'process.env.NODE_ENV': JSON.stringify(mode),
+          },
+          resolve: {
+                  alias: {
+                            '@': path.resolve(__dirname, './src'),
+                  },
+          },
+          build: {
+                  outDir: 'dist',
+                  assetsDir: 'assets',
+                  emptyOutDir: true
+          },
+          server: {
+                  port: 3000,
+                  host: '0.0.0.0',
+                  hmr: process.env.DISABLE_HMR !== 'true',
+          },
+    };
 });
